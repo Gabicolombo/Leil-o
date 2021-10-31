a9009c527f9274eefe0b4e90227278b3b7447622
Silas Reis <silas.reis.bovolin@gmail.com>
2021-10-31 14:42:36 -0300

    tirando o comentario

--- src/server.js
+++ src/server.js
@@ -4,17 +4,17 @@ const { Socket } = require('./socket');
 
 const port = process.env.PORT || 2828;
 
-// cron.schedule('*/5 * * * * *', () => {
-//   const rooms = Socket.rooms;
-//   Object.keys(rooms).forEach(async (key) => {
-//     const { startDate } = rooms[key];
-//     if (new Date() > new Date(startDate)) {
-//       console.log('começou', key);
-//       await Socket.decrementTime(key);
-//     }
-//   });
-//   console.log(Socket.rooms);
-// });
+cron.schedule('* * * * *', () => {
+  const rooms = Socket.rooms;
+  Object.keys(rooms).forEach(async (key) => {
+    const { startDate } = rooms[key];
+    if (new Date() > new Date(startDate)) {
+      console.log('começou', key);
+      await Socket.decrementTime(key);
+    }
+  });
+  console.log(Socket.rooms);
+});
 
 serverHttp.listen(port, () => console.log(`Server is running on PORT ${port}`));
 
